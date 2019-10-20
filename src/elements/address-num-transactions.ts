60d682a09198ad6a91b4f06e88b244b0cfa76796
mohoff <moritz.hoffmann@brickblock.io>
2019-10-20 19:59:18 +0200

    fix: number of tx of an address is nonce+1 instead of nonce

--- src/elements/address-num-transactions.ts
+++ src/elements/address-num-transactions.ts
@@ -2,4 +2,4 @@ import numeral from 'numeral'
 import chalk from 'chalk'
 
 export const getNumberOfTransactions = (nonce: number): string =>
-  chalk.grey(numeral(nonce).format('0o') + ' tx')
+  chalk.grey(numeral(nonce + 1).format('0o') + ' tx')
